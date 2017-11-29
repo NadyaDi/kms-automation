ddfba2323bf0e69c77400fba2b26a1f70c7e0448
Sigalov <sigalov.oleg@gmail.com>
2017-11-29 17:08:51 +0200

    git helper

--- git-helper/CreateNewBranch.bat
+++ git-helper/CreateNewBranch.bat
@@ -26,12 +26,10 @@ echo Version number:  %id%
 
 ::Saving number in text file for next branch version
 echo %id% > branch.txt
-
-Cd "D:\Work\Git Projects\kms-automation"
-"C:\Program Files\Git\bin\git.exe" reset --hard
-"C:\Program Files\Git\bin\git.exe" checkout master
-"C:\Program Files\Git\bin\git.exe" pull origin master
-"C:\Program Files\Git\bin\git.exe" checkout -b OlegKmsPy-%id%
+"C:\Program Files (x86)\Git\bin\git.exe" reset --hard
+"C:\Program Files (x86)\Git\bin\git.exe" checkout master
+"C:\Program Files (x86)\Git\bin\git.exe" pull origin master
+"C:\Program Files (x86)\Git\bin\git.exe" checkout -b KmsGoOleg-%id%
 
 :end
 Pause
