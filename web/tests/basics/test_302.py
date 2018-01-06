f3e5ce0c55cf6f35ae17e6a5723c7ce4007cf413
Sigalov <sigalov.oleg@gmail.com>
2018-01-06 16:21:21 +0200

    new changes

--- web/tests/basics/test_302.py
+++ web/tests/basics/test_302.py
@@ -25,6 +25,8 @@ class Test:
     timeout_accured = "False"
     driver = None
     
+    filePath = "C:\\TestComplete\\automation-tests\\KalturaCore\\TestData\\Videos\\Images\\automation.jpg"
+    
     #run test as different instances on all the supported platforms
     @pytest.fixture(scope='module',params=supported_platforms)
     def driverFix(self,request):
@@ -40,23 +42,28 @@ class Test:
             #capture test start time
             self.startTime = time.time()
             #initialize all the basic vars and start playing
-            self,capture,driver = clsTestService.initializeAndLoginAsUser(self, driverFix)
-#             self,captur,self.driver = clsTestService.initialize(self, driverFix)
-            ##################################################################
-            
-            ################### TEST REUSABLE CLASS HELPERS ##################
+            self,captur,self.driver = clsTestService.initialize(self, driverFix)
             common = Common(self.driver)
-            ##################################################################
 
             ##################### TEST STEPS - MAIN FLOW #####################
-#             writeToLog("INFO","Step 1: Going to perform login to KMS site as user")
-#             if common.loginAsUser() == False:
-#                 self.status = "Fail"
-#                 writeToLog("INFO","Step 1: FAILED")
-#                 return
-#
-#             common.entryPage.navigateToEntryPage('RedGreenBlueImage.png')
-            common.upload.uploadEntry("filePath", "namename", "descritiondescrition", "tags1,tags2,")
+            writeToLog("INFO","Step 1: Going to perform login to KMS site as user")
+            if common.loginAsUser() == False:
+                self.status = "Fail"
+                writeToLog("INFO","Step 1: FAILED to login as user")
+                return
+            
+            writeToLog("INFO","Step 2: Going to upload entry")
+            if common.upload.uploadEntry(self.filePath, "My Entry Name", "descritiondescrition", "tags1,tags2,") == False:
+                self.status = "Fail"
+                writeToLog("INFO","Step 2: FAILED failed to upload entry")
+                return
+                   
+            writeToLog("INFO","Step 3: Going to navigate to Entry Page")
+            if common.entryPage.navigateToEntryPage('My Entry Name') == False:
+                self.status = "Fail"
+                writeToLog("INFO","Step 3: FAILED navigate to Entry Page")
+                return                        
+            
             ##################################################################
             print("DONE")
         # if an exception happened we need to handle it and fail the test       
