62cfd2b75ce44b216da8654d5d8a5c5047ebc138
unknown <Michal.Zomper@IL-MICHALZ-QA.Kaltura.com>
2018-01-15 17:57:55 +0200

    add logout function finished add addCollaborator function

--- web/tests/basics/test_345.py
+++ web/tests/basics/test_345.py
@@ -48,32 +48,43 @@ class Test:
             #capture test start time
             self.startTime = time.time()
             #initialize all the basic vars and start playing
-            self,captur,self.driver = clsTestService.initialize(self, driverFix)
+            self,capture,self.driver = clsTestService.initialize(self, driverFix)
             self.common = Common(self.driver)
-            self.entryName = clsTestService.addGuidToString('entryName')
+            self.entryName = clsTestService.addGuidToString('Michal entry ')
+            self.newUserId = "Automation_User_1"
+            self.newUserPass = "123456"
             ##################### TEST STEPS - MAIN FLOW #####################
             writeToLog("INFO","Step 1: Going to perform login to KMS site as user")
             if self.common.loginAsUser() == False:
                 self.status = "Fail"
                 writeToLog("INFO","Step 1: FAILED to login as user")
                 return
-             
+            
             writeToLog("INFO","Step 2: Going to upload entry")
-            if self.common.upload.uploadEntry(self.filePath, self.entryName, "descritiondescrition", "tags1,tags2,") == False:
+            if self.common.upload.uploadEntry(self.filePath, self.entryName, "description  description", "tags1,tags2,") == False:
                 self.status = "Fail"
                 writeToLog("INFO","Step 2: FAILED failed to upload entry")
                 return
             
+            writeToLog("INFO","Step 3: Going to add Collaborator in edit Entry Page")
             if self.common.editEntryPage.addCollaborator(self.entryName, self.newUserId, True, False):
                 self.status = "Fail"
                 writeToLog("INFO","Step 3: FAILED failed to add user as a collaborator")
                 return
                     
-            writeToLog("INFO","Step 3: Going to navigate to Entry Page")
-            if self.common.entryPage.navigateToEntryPage(self.entryName) == False:
+            writeToLog("INFO","Step 4: Going to logout from main user")
+            if self.common.login.LogOutOfKMS() == False:
                 self.status = "Fail"
-                writeToLog("INFO","Step 3: FAILED navigate to Entry Page")
-                return
+                writeToLog("INFO","Step 4: FAILED failed to logout from main user")
+                return  
+                              
+            writeToLog("INFO","Step 5: Going to login with the user that was added as Collaborator")
+            if self.common.login.loginToKMS(self.newUserId, self.newUserPass) == False:
+                self.status = "Fail"
+                writeToLog("INFO","Step 5: FAILED to login with the user that was added as Collaborator")
+                return     
+            
+                   
             ##################################################################
             print("DONE")
         # if an exception happened we need to handle it and fail the test       
