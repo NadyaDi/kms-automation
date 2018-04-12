ec49a4a891564697a7135d28d874f3f9c3253796
Sigalov <sigalov.oleg@gmail.com>
2018-04-12 14:58:25 +0300

    Autoit fix

--- web/lib/autoit.py
+++ web/lib/autoit.py
@@ -10,5 +10,9 @@ class Autoit():
     def __init__(self, clsCommon):
         if localSettings.LOCAL_SETTINGS_RUN_MDOE == localSettings.REMOTE_RUN_MODE:
             self.clsCommon = clsCommon
-            self.autoitDriver = webdriver.Remote( command_executor='http://' + localSettings.LOCAL_SETTINGS_AUTOIT_SERVICE_HOST + '/wd/hub', desired_capabilities={'browserName':'AutoIt'})      
-            self.autoitDriver2 = webdriver.Remote( command_executor='http://' + localSettings.LOCAL_SETTINGS_AUTOIT_SERVICE_HOST2 + '/wd/hub', desired_capabilities={'browserName':'AutoIt'})
+            if localSettings.LOCAL_SETTINGS_SELENIUM_GRID_POOL == "qaKmsFrontEnd":
+                self.autoitDriver = webdriver.Remote( command_executor='http://' + localSettings.LOCAL_SETTINGS_AUTOIT_SERVICE_HOST + '/wd/hub', desired_capabilities={'browserName':'AutoIt'})      
+            elif localSettings.LOCAL_SETTINGS_SELENIUM_GRID_POOL == "qaKmsFrontEnd2":
+                self.autoitDriver2 = webdriver.Remote( command_executor='http://' + localSettings.LOCAL_SETTINGS_AUTOIT_SERVICE_HOST2 + '/wd/hub', desired_capabilities={'browserName':'AutoIt'})
+            else:
+                writeToLog("INFO","FAIELD to connect to Autoit service on remote host: " + localSettings.LOCAL_SETTINGS_SELENIUM_GRID_POOL)
