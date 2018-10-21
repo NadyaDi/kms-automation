5e0b497eca205ab2f6d1f0834bb8b780404224e1
Ori Flchtman <ori.flchtman@Kaltura.com>
2018-10-21 12:00:36 +0300

    Start new test 679

--- web/lib/myPlaylists.py
+++ web/lib/myPlaylists.py
@@ -141,6 +141,106 @@ class MyPlaylists(Base):
             return False
             
         return True
+    
+    
+#         def addMultipleEntriesToPlaylistRemoveFromOldPlaylist(self, entryName, playlistName='', toCreateNewPlaylist = False, currentLocation = enums.Location.MY_MEDIA):
+#         try:
+#             if currentLocation == enums.Location.MY_MEDIA: 
+#                 if self.clsCommon.myMedia.navigateToMyMedia(forceNavigate=True) == False:
+#                     writeToLog("INFO","FAILED to navigate to my media")
+#                     return False
+#                 
+#                 if type(entryName) is list: 
+#                     if self.clsCommon.myMedia.checkEntriesInMyMedia(entryName) == False:
+#                         writeToLog("INFO","FAILED to check entries in My-Media")
+#                         return False
+#                 else: 
+#                     if self.clsCommon.myMedia.serachAndCheckSingleEntryInMyMedia(entryName) == False:
+#                         writeToLog("INFO","FAILED to check entry '" + entryName + "' check box")
+#                         return False
+#              
+#                 if self.clsCommon.myMedia.clickActionsAndAddToPlaylistFromMyMedia() == False:
+#                     writeToLog("INFO","FAILED to click on action button")
+#                     return False
+#                     sleep(7)  
+#                       
+#             elif currentLocation == enums.Location.ENTRY_PAGE: 
+#                 sleep(1)
+#                 # Click on action tab
+#                 if self.click(self.clsCommon.entryPage.ENTRY_PAGE_ACTIONS_DROPDOWNLIST, 30) == False:
+#                     writeToLog("INFO","FAILED to click on action button in entry page '" + entryName + "'")
+#                     return False  
+#                 
+#                 sleep(1)
+#                 # Click on publish button
+#                 if self.click(self.clsCommon.entryPage.ENTRY_PAGE_ADDTOPLAYLIST_BUTTON, 30) == False:
+#                     writeToLog("INFO","FAILED to click on publish button in entry page '" + entryName + "'")
+#                     return False
+#             
+#             else:
+#                 writeToLog("INFO","FAILED, Add entry to playlist: the provided ""currentLocation"" Value is not accepted ")
+#                 return False
+#             
+#             
+#             if toCreateNewPlaylist != False:
+#                 self.clear_and_send_keys(self.CREATE_PLAYLIST_TEXT_FIELD, playlistName)
+#                 sleep(1)
+#             
+#                 if self.click(self.CREATE_PLAYLIST_CREATE_BUTTON) == False:
+#                     writeToLog("INFO","FAILED to click on create playlist Button")
+#                     return False
+#                 
+#                 self.clsCommon.general.waitForLoaderToDisappear()
+#                 
+#                 sleep(1)
+#                 if self.click(self.CREATE_PLAYLIST_SAVE_BUTTON) == False:
+#                     writeToLog("INFO","FAILED to click on create playlist Button")
+#                     return False
+#                 
+#                 if currentLocation == enums.Location.MY_MEDIA:     
+#                     if self.wait_visible(self.CREATE_PLAYLIST_CONFIRM_MSG, 10) == False:
+#                         writeToLog("INFO","FAILED to to create playlist: " + playlistName)
+#                         return False
+#                 
+#                 else:
+#                     tmp_success_msg =(self.CREATE_PLAYLIST_CONFIRM_MSG_ENTRY_PAGE[0], self.CREATE_PLAYLIST_CONFIRM_MSG_ENTRY_PAGE[1].replace('PLAYLIST_NAME', playlistName))
+#                     if self.wait_visible(tmp_success_msg, 10) == False:
+#                         writeToLog("INFO","FAILED to to create playlist: " + playlistName)
+#                         return False
+#                 
+#                 writeToLog("INFO","Playlist: '" + playlistName + "' successfully created")
+#             
+#             else:
+#                 tmp_playlist_name = (self.PLAYLIST_CHECKBOX[0], self.PLAYLIST_CHECKBOX[1].replace('PLAYLIST_NAME', playlistName))   
+#                 if self.click(tmp_playlist_name) == False:
+#                     writeToLog("INFO","FAILED to Check for playlist: '" + playlistName + "' something went wrong")
+#                     return False
+#                 
+#                 if self.click(self.CREATE_PLAYLIST_SAVE_BUTTON) == False:
+#                     writeToLog("INFO","FAILED to click on create playlist Button")
+#                     return False
+#                 
+#                 self.clsCommon.general.waitForLoaderToDisappear()
+#                 sleep(1)
+#                 
+#                 if currentLocation == enums.Location.MY_MEDIA:
+#                     if self.wait_visible(self.CREATE_PLAYLIST_CONFIRM_MSG, 10) == False:
+#                         writeToLog("INFO","FAILED to to create playlist: " + playlistName)
+#                         return False
+#                 
+#                 else:
+#                     tmp_success_msg =(self.CREATE_PLAYLIST_CONFIRM_MSG_ENTRY_PAGE[0], self.CREATE_PLAYLIST_CONFIRM_MSG_ENTRY_PAGE[1].replace('PLAYLIST_NAME', playlistName))
+#                     if self.wait_visible(tmp_success_msg, 10) == False:
+#                         writeToLog("INFO","FAILED to to create playlist: " + playlistName)
+#                         return False
+#                 
+#                 writeToLog("INFO","Entry: """ + str(entryName) + """ added to Playlist: """ + playlistName + "")
+#                                        
+#         except NoSuchElementException:
+#             return False
+#             
+#         return True
+    
 
     #  @Author: Tzachi Guetta
     def addEntriesToPlaylist(self, entriesName, playlistName, toCreateNewPlaylist):
---
+++ web/tests/playlist/test_679.py
@@ -0,0 +1,120 @@
+import sys,os
+sys.path.insert(1,os.path.abspath(os.path.join(os.path.dirname( __file__ ),'..','..','lib')))
+import time, pytest
+from clsCommon import Common
+import clsTestService
+import enums
+from localSettings import *
+import localSettings
+from utilityTestFunc import *
+
+
+class Test:
+    
+    #================================================================================================================================
+    #  @Author: Ori Flchtman
+    # Test description:
+    # Add Video/ Audio/ Image Remove Entry from one of playlist during to add same Entry to new 
+    # The test's Flow: 
+    # Login to KMS-> Upload entries (Video/ Audio/ Image) 
+    # add entries to playlist-> enter each entry-> Actions-> Add to Playlist-> Create new Playlist and uncheck current playlist-> Save->  
+    # Verify messages of Create new Playlist, Add to Playlit and remove from playlist
+    #================================================================================================================================
+    testNum     = "679"
+    enableProxy = False
+    
+    supported_platforms = clsTestService.updatePlatforms(testNum)
+    
+    status = "Pass"
+    timeout_accured = "False"
+    driver = None
+    common = None
+    # Test variables
+    entriesList = []
+    entryDescription = "description"
+    entryTags = "tag1,"
+    filePath1 = localSettings.LOCAL_SETTINGS_MEDIA_PATH + r'\images\automation2.jpg'
+    filePathVideo = localSettings.LOCAL_SETTINGS_MEDIA_PATH + r'\videos\QR30SecMidRight.mp4' 
+    filePathAudio = localSettings.LOCAL_SETTINGS_MEDIA_PATH + r'\audios\audio.mp3' 
+    filePathImage = localSettings.LOCAL_SETTINGS_MEDIA_PATH + r'\images\AutomatedBenefits.jpg'   
+    #run test as different instances on all the supported platforms
+    @pytest.fixture(scope='module',params=supported_platforms)
+    def driverFix(self,request):
+        return request.param  
+    
+    def test_01(self,driverFix,env):
+
+        try:
+            logStartTest(self,driverFix)
+            ############################# TEST SETUP ###############################
+            #capture test start time
+            self.startTime = time.time()
+            #initialize all the basic vars and start playing
+            self,self.driver = clsTestService.initializeAndLoginAsUser(self, driverFix)
+            self.common = Common(self.driver)        
+            ########################################################################
+            self.entryName1 = clsTestService.addGuidToString('Entry1', self.testNum)
+            self.entryName2 = clsTestService.addGuidToString('EntryVideo', self.testNum)
+            self.entryName3 = clsTestService.addGuidToString('EntryAudio', self.testNum)
+            self.entryName4 = clsTestService.addGuidToString('EntryImage', self.testNum)             
+            self.entriesList = [self.entryName1, self.entryName2, self.entryName3, self.entryName4]
+            self.playlistName1 = clsTestService.addGuidToString('emptyPlaylistTest#1', self.testNum)
+            self.playlistName2 = clsTestService.addGuidToString('PlaylistTest#2', self.testNum)
+            self.listOfPlaylists = [self.playlistName1, self.playlistName2]
+            ########################## TEST STEPS - MAIN FLOW ####################### 
+            self.entriesToUpload = {
+                self.entryName1: self.filePath1, 
+                self.entryName2: self.filePathVideo,
+                self.entryName3: self.filePathAudio,
+                self.entryName4: self.filePathImage}                      
+            
+            writeToLog("INFO","Step 1: Going to upload 4 entries")
+            if self.common.upload.uploadEntries(self.entriesToUpload, self.entryDescription, self.entryTags) == False:
+                self.status = "Fail"
+                writeToLog("INFO","Step 1: FAILED to upload 4 entries")
+                return
+            
+            self.common.myMedia.navigateToMyMedia(forceNavigate=True)
+            writeToLog("INFO","Step 2: Going to create playlist with 3 Entries")
+            if self.common.myPlaylists.addSingleEntryToPlaylist(self.entriesList, toCreateNewPlaylist=True) == False:
+                self.status = "Fail"
+                writeToLog("INFO","Step 2: FAILED to create playlist with 3 Entries ")
+                return
+            
+#             self.common.myMedia.navigateToMyMedia(forceNavigate=True)     
+# #           Need to write a new function that will create new playlist but also uncheck current playlist
+#             writeToLog("INFO","Step 3: Going to create playlist with 3 Entries and remove entries from previous playlist")
+#             if self.common.myPlaylists.addMultipleEntriesToPlaylistRemoveFromOldPlaylist(self.entriesList, toCreateNewPlaylist=True) == False:
+#                 self.status = "Fail"
+#                 writeToLog("INFO","Step 3: FAILED to create playlist with 3 Entries and remove entries from previous playlist ")
+#                 return         
+#              
+#             writeToLog("INFO","Step 8: Going to verify Entry in Playlist1")
+#             if self.common.myPlaylists.verifyMultipleEntriesInMultiplePlaylists(self.entriesList, self.listOfPlaylists) == False:
+#                 self.status = "Fail"
+#                 writeToLog("INFO","Step 8: FAILED to verify Entry in Playlist1")
+#                 return
+                                                                      
+            #########################################################################
+            writeToLog("INFO","TEST PASSED")
+        # If an exception happened we need to handle it and fail the test       
+        except Exception as inst:
+            self.status = clsTestService.handleException(self,inst,self.startTime)
+            
+    ########################### TEST TEARDOWN ###########################    
+    def teardown_method(self,method):
+        try:
+            self.common.handleTestFail(self.status)              
+            writeToLog("INFO","**************** Starting: teardown_method **************** ")
+            self.common.myMedia.deleteEntriesFromMyMedia(self.entriesList)
+            self.common.myPlaylists.deletePlaylist(self.playlistName1)
+            self.common.myPlaylists.deletePlaylist(self.playlistName2)            
+            writeToLog("INFO","**************** Ended: teardown_method *******************")
+        except:
+            pass            
+        clsTestService.basicTearDown(self)
+        #write to log we finished the test
+        logFinishedTest(self,self.startTime)
+        assert (self.status == "Pass")    
+
+    pytest.main('test_' + testNum  + '.py --tb=line')
