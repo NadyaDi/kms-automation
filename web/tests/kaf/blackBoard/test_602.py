db00e5bb34e037a65c72770f872e4c215c9f6add
Michal Zomper <michal.zomper@Kaltura.com>
2018-11-13 19:12:37 +0200

    started working on test 602 - BlackBoard - Preview all kinds of media from My Media and Gallery not finished

--- web/tests/kaf/blackBoard/test_602.py
+++ web/tests/kaf/blackBoard/test_602.py
@@ -1,27 +1,28 @@
-import time, pytest
 import sys,os
 sys.path.insert(1,os.path.abspath(os.path.join(os.path.dirname( __file__ ),'..','..','lib')))
+from enum import *
+import time, pytest
 from clsCommon import Common
 import clsTestService
+import enums
 from localSettings import *
 import localSettings
 from utilityTestFunc import *
-import enums
+import ctypes
 
 
 class Test:
-    
     #================================================================================================================================
-    #  @Author: Michal Zomper
-    # Test Name : preview all kinds of media from My Media amd Category
+    # @Author: Michal Zomper
+    # Test Name : BlackBoard - Preview all kinds of media from My Media and Gallery
     # Test description:
     # upload 3 entries : video / Audio / Image
-    #  Navigate to each entry page and verify player is working  and that the video / audio / image  is correct
-    # Publish the entries to category. navigate to entries form the category and verify the player  
+    # Navigate to each entry page and verify player is working  and that the video / audio / image  is correct
     #================================================================================================================================
-    testNum = "644"
-    
+    testNum     = "602"
+    application = enums.Application.BLACK_BOARD
     supported_platforms = clsTestService.updatePlatforms(testNum)
+    
     
     status = "Pass"
     timeout_accured = "False"
@@ -41,7 +42,7 @@ class Test:
     vidoeLength = "0:10"
     audioLength = "0:30"
     vidoeTimeToStop = "0:07"
-    categoryName = None
+    galleryName = "New1"
     
     #run test as different instances on all the supported platforms
     @pytest.fixture(scope='module',params=supported_platforms)
@@ -50,9 +51,8 @@ class Test:
     
     def test_01(self,driverFix,env):
 
-        #write to log we started the test
-        logStartTest(self,driverFix)
         try:
+            logStartTest(self, driverFix, self.application)
             ########################### TEST SETUP ###########################
             #capture test start time
             self.startTime = time.time()
@@ -62,7 +62,6 @@ class Test:
             self.videoEntryName = clsTestService.addGuidToString("Upload media and verify in player - Video", self.testNum)
             self.audioEntryName = clsTestService.addGuidToString("Upload media and verify in player - Audio", self.testNum)
             self.imageEntryName = clsTestService.addGuidToString("Upload media and verify in player - Image", self.testNum)
-            self.categoryName = clsTestService.addGuidToString('Upload media and verify in player category', self.testNum)
             
             self.entriesToUpload = {
             self.videoEntryName: self.filePathVideo, 
@@ -70,51 +69,50 @@ class Test:
             self.imageEntryName: self.filePathImage }
             
             ##################### TEST STEPS - MAIN FLOW ##################### 
+            self.common.channel.searchInChannelWithoutVerifyResults("Automation Entry - Published")
             
-            writeToLog("INFO","Step 1: Going to create parent category") 
-            self.common.apiClientSession.startCurrentApiClientSession()
-            parentId = self.common.apiClientSession.getParentId('galleries') 
-            if self.common.apiClientSession.createCategory(parentId, localSettings.LOCAL_SETTINGS_LOGIN_USERNAME, self.categoryName, self.description) == False:
-                self.status = "Fail"
-                writeToLog("INFO","Step 1: FAILED to create parent category")
-                return
-             
-            writeToLog("INFO","Step 2: Going to clear cache")            
-            if self.common.admin.clearCache() == False:
-                self.status = "Fail"
-                writeToLog("INFO","Step 2: FAILED to clear cache")
-                return
-            
-            writeToLog("INFO","Step 3: Going navigate to home page")            
-            if self.common.home.navigateToHomePage(forceNavigate=True) == False:
-                self.status = "Fail"
-                writeToLog("INFO","Step 3: FAILED navigate to home page")
-                return
-            
-            writeToLog("INFO","Step 4: Going to upload 3 entries: Video / Audio / Image")   
+            writeToLog("INFO","Step 1: Going to upload 3 entries: Video / Audio / Image")   
             if self.common.upload.uploadEntries(self.entriesToUpload, self.description, self.tags) == False:
                 self.status = "Fail"
-                writeToLog("INFO","Step 4: FAILED to upload 3 entries")
+                writeToLog("INFO","Step 1: FAILED to upload 3 entries")
                 return
                     
-            writeToLog("INFO","Step 5: Going navigate to image entry: "+ self.imageEntryName)    
+            writeToLog("INFO","Step 2: Going navigate to image entry: "+ self.imageEntryName)    
             if self.common.entryPage.navigateToEntry(self.imageEntryName, enums.Location.MY_MEDIA) == False:
                 self.status = "Fail"
-                writeToLog("INFO","Step 5: FAILED navigate to entry: " + self.imageEntryName)
+                writeToLog("INFO","Step 2: FAILED navigate to entry: " + self.imageEntryName)
                 return 
                 
-            writeToLog("INFO","Step 6: Going to verify the entry in player")            
+            writeToLog("INFO","Step 3: Going to verify the entry in player")            
             if self.common.entryPage.verifyEntryViaType(enums.MediaType.IMAGE, "", "", self.ImageQrCodeResult) == False:
                 self.status = "Fail"
-                writeToLog("INFO","Step 6: FAILED to verify the entry '" + self.imageEntryName + "' in player")
+                writeToLog("INFO","Step 3: FAILED to verify the entry '" + self.imageEntryName + "' in player")
                 return   
             
-            writeToLog("INFO","Step 7: Going navigate to audio entry: "+ self.audioEntryName)    
+            writeToLog("INFO","Step 4: Going navigate to audio entry: "+ self.audioEntryName)    
             if self.common.entryPage.navigateToEntry(self.audioEntryName, enums.Location.MY_MEDIA) == False:
                 self.status = "Fail"
-                writeToLog("INFO","Step 7: FAILED navigate to entry: " + self.audioEntryName)
+                writeToLog("INFO","Step 4: FAILED navigate to entry: " + self.audioEntryName)
                 return 
                         
+            writeToLog("INFO","Step 5: Going to wait until media will finish processing")
+            if self.common.entryPage.waitTillMediaIsBeingProcessed() == False:
+                self.status = "Fail"
+                writeToLog("INFO","Step 5: FAILED - New entry is still processing")
+                return 
+            
+            writeToLog("INFO","Step 6: Going to verify the entry in player")            
+            if self.common.entryPage.verifyEntryViaType(enums.MediaType.AUDIO, self.audioLength, "", "") == False:
+                self.status = "Fail"
+                writeToLog("INFO","Step 6: FAILED to verify the entry '" + self.audioEntryName + "' in player")
+                return   
+   
+            writeToLog("INFO","Step 7: Going navigate to video entry: "+ self.videoEntryName)    
+            if self.common.entryPage.navigateToEntry(self.videoEntryName, enums.Location.MY_MEDIA) == False:
+                self.status = "Fail"
+                writeToLog("INFO","Step 7: FAILED navigate to entry: " + self.videoEntryName)
+                return 
+                  
             writeToLog("INFO","Step 8: Going to wait until media will finish processing")
             if self.common.entryPage.waitTillMediaIsBeingProcessed() == False:
                 self.status = "Fail"
@@ -122,39 +120,26 @@ class Test:
                 return 
             
             writeToLog("INFO","Step 9: Going to verify the entry in player")            
-            if self.common.entryPage.verifyEntryViaType(enums.MediaType.AUDIO, self.audioLength, "", "") == False:
-                self.status = "Fail"
-                writeToLog("INFO","Step 9: FAILED to verify the entry '" + self.audioEntryName + "' in player")
-                return   
-   
-            writeToLog("INFO","Step 10: Going navigate to video entry: "+ self.videoEntryName)    
-            if self.common.entryPage.navigateToEntry(self.videoEntryName, enums.Location.MY_MEDIA) == False:
-                self.status = "Fail"
-                writeToLog("INFO","Step 10: FAILED navigate to entry: " + self.videoEntryName)
-                return 
-                  
-            writeToLog("INFO","Step 11: Going to wait until media will finish processing")
-            if self.common.entryPage.waitTillMediaIsBeingProcessed() == False:
-                self.status = "Fail"
-                writeToLog("INFO","Step 11: FAILED - New entry is still processing")
-                return 
-            
-            writeToLog("INFO","Step 12: Going to verify the entry in player")            
             if self.common.entryPage.verifyEntryViaType(enums.MediaType.VIDEO, self.vidoeLength, self.vidoeTimeToStop, self.videoQrCodeResult) == False:
                 self.status = "Fail"
-                writeToLog("INFO","Step 12: FAILED to verify the entry '" + self.videoEntryName + "' in player")
+                writeToLog("INFO","Step 9: FAILED to verify the entry '" + self.videoEntryName + "' in player")
                 return   
             
+            
+            
+            
+            
+            
             writeToLog("INFO","Step 13: Going to publish entries to category")
-            if self.common.myMedia.publishEntriesFromMyMedia([self.videoEntryName,self.audioEntryName,self.imageEntryName], [self.categoryName], '', showAllEntries=True) == False:
+            if self.common.myMedia.publishEntriesFromMyMedia([self.videoEntryName,self.audioEntryName,self.imageEntryName], [self.galleryName], '', showAllEntries=True) == False:
                 self.status = "Fail"
                 writeToLog("INFO","Step 13: FAILED to publish entries to category")
                 return  
             
-            writeToLog("INFO","Step 14: Going navigate to image entry: '" + self.imageEntryName + "' from category page")    
-            if self.common.entryPage.navigateToEntry(self.imageEntryName, enums.Location.CATEGORY_PAGE, categoryName=self.categoryName) == False:
+            writeToLog("INFO","Step 14: Going navigate to image entry: '" + self.imageEntryName + "' from gallery page")    
+            if self.common.blackBoard.navigateToEntryPageFromBBGalleryPage(self.imageEntryName, self.galleryName) == False:
                 self.status = "Fail"
-                writeToLog("INFO","Step 14: FAILED navigate to entry: " + self.imageEntryName + " from category page")
+                writeToLog("INFO","Step 14: FAILED navigate to entry: " + self.imageEntryName + " from gallery page")
                 return 
                 
             writeToLog("INFO","Step 15: Going to verify the entry in player")            
@@ -163,10 +148,10 @@ class Test:
                 writeToLog("INFO","Step 15: FAILED to verify the entry '" + self.imageEntryName + "' in player")
                 return   
             
-            writeToLog("INFO","Step 16: Going navigate to audio entry: '" + self.audioEntryName + "' from category page")    
-            if self.common.entryPage.navigateToEntry(self.audioEntryName, enums.Location.CATEGORY_PAGE, categoryName=self.categoryName) == False:
+            writeToLog("INFO","Step 16: Going navigate to audio entry: '" + self.audioEntryName + "' from gallery page")    
+            if self.common.blackBoard.navigateToEntryPageFromBBGalleryPage(self.audioEntryName, self.galleryName) == False:
                 self.status = "Fail"
-                writeToLog("INFO","Step 16: FAILED navigate to entry: " + self.audioEntryName + " from category page")
+                writeToLog("INFO","Step 16: FAILED navigate to entry: " + self.audioEntryName + " from gallery page")
                 return 
             
             writeToLog("INFO","Step 17: Going to verify the entry in player")            
@@ -175,10 +160,10 @@ class Test:
                 writeToLog("INFO","Step 17: FAILED to verify the entry '" + self.audioEntryName + "' in player")
                 return   
             
-            writeToLog("INFO","Step 18: Going navigate to video entry: '"+ self.videoEntryName + "' from category page")    
-            if self.common.entryPage.navigateToEntry(self.videoEntryName, enums.Location.CATEGORY_PAGE, categoryName=self.categoryName) == False:
+            writeToLog("INFO","Step 18: Going navigate to video entry: '"+ self.videoEntryName + "' from gallery page")    
+            if self.common.blackBoard.navigateToEntryPageFromBBGalleryPage(self.videoEntryName, self.galleryName) == False:
                 self.status = "Fail"
-                writeToLog("INFO","Step 101: FAILED navigate to entry: " + self.videoEntryName + " from category page")
+                writeToLog("INFO","Step 101: FAILED navigate to entry: " + self.videoEntryName + " from gallery page")
                 return 
             
             writeToLog("INFO","Step 19: Going to verify the entry in player")            
@@ -186,8 +171,9 @@ class Test:
                 self.status = "Fail"
                 writeToLog("INFO","Step 19: FAILED to verify the entry '" + self.videoEntryName + "' in player")
                 return 
+              
             ##################################################################
-            writeToLog("INFO","TEST PASSED: 'Preview all kinds of media from My Media and category' was done successfully")
+            writeToLog("INFO","TEST PASSED: 'BlackBoard - Preview all kinds of media from My Media and Gallery' was done successfully")
         # if an exception happened we need to handle it and fail the test       
         except Exception as inst:
             self.status = clsTestService.handleException(self,inst,self.startTime)
@@ -198,7 +184,6 @@ class Test:
             self.common.handleTestFail(self.status)
             writeToLog("INFO","**************** Starting: teardown_method ****************")      
             self.common.myMedia.deleteEntriesFromMyMedia([self.videoEntryName, self.audioEntryName, self.imageEntryName])
-            self.common.apiClientSession.deleteCategory(self.categoryName)
             writeToLog("INFO","**************** Ended: teardown_method *******************")            
         except:
             pass            
