2f982d321e95f70621949ec477443b290cdae327
Oleg Sigalov <oleg.sigalov@Kaltura.com>
2019-02-25 15:32:37 +0200

    Fix for test 638 (Global Search - Search for Media)

--- web/lib/globalSearch.py
+++ web/lib/globalSearch.py
@@ -95,69 +95,43 @@ class  GlobalSearch(Base):
     # the function verify the matedata for the searched entry in the global search 
     def VerifyEntryMetadataAfterGlobalSearch(self, entryName, thumbQRCodeResult, description):
         sleep(2)
-        if localSettings.LOCAL_SETTINGS_IS_NEW_UI == True:
-            # Verify thumbnail qr code
-            result =  self.clsCommon.myMedia.getResultAfterSearch(entryName)
-            if result == False:
-                writeToLog("INFO","FAILED to find search word in global search")
-                return False
-            
-            sleep(1)
-            parent = result.find_element_by_xpath("..")
-            entryHref = parent.get_attribute("href")
-            entryId = entryHref.split("/")[len(entryHref.split("/"))-1]
-            
-            tmp_entryThum = (self.ENTRY_THUMBNAIL_AFTER_GLOBAL_SEARCH_IMG[0], self.ENTRY_THUMBNAIL_AFTER_GLOBAL_SEARCH_IMG[1].replace('ENTRY_ID', entryId))
-            
-            thumbElement = self.wait_element(tmp_entryThum, multipleElements=True)
-            if thumbElement == False:
-                writeToLog("INFO","FAILED to find entry thumbnail element after global search")
-                return False                
-            
-            sleep(2)
-            thumQrCode =  self.clsCommon.qrcode.resolveQrCode(self.clsCommon.qrcode.takeCustomQrCodeScreenshot(8.7,2.8,4.4,2))
-             
-            if thumQrCode != thumbQRCodeResult:
-                writeToLog("INFO","FAILED verify entry thumbnail QR code after global search")
-                return False
-            
-            # Verify entry description
-            tmp_entrydesc = (self.ENTRY_PARENT_DESCRIPTION_AFTER_GLOBAL_SEARCH_NEWUI[0], self.ENTRY_PARENT_DESCRIPTION_AFTER_GLOBAL_SEARCH_NEWUI[1].replace('ENTRY_ID', entryId))
-            try:
-                descriptionPerantElement = self.get_element(tmp_entrydesc)
-                descriptionElement = self.get_child_element(descriptionPerantElement, self.ENTRY_DESCRIPTION_AFTER_GLOBAL_SEARCH_NEWUI)
-            except NoSuchElementException:
-                writeToLog("INFO","FAILED to find entry description element after global search")
-                return False
-            
-            if descriptionElement.text != description:
-                writeToLog("INFO","FAILED to verify entry description after global search")
-                return False
-        else:
-            # Verify thumbnail qr code
-            tmp_entryThum = (self.ENTRY_THUMBNAIL_AFTER_GLOBAL_SEARCH_OLDUI[0], self.ENTRY_THUMBNAIL_AFTER_GLOBAL_SEARCH_OLDUI[1].replace('ENTRY_NAME', entryName))
-            try:
-                thumbElement = self.get_element(tmp_entryThum)
-            except NoSuchElementException:
-                writeToLog("INFO","FAILED to find entry thumbnail element after global search")
-                return False
-            
-            thumQrCode =  self.clsCommon.qrcode.takeAndResolveElementQrCodeScreenshot(thumbElement) 
-            if thumQrCode != thumbQRCodeResult:
-                writeToLog("INFO","FAILED verify entry thumbnail QR code after global search")
-                return False
-            
-            
-            try:
-                entryParentel = thumbElement.find_element_by_xpath("../../../..")
-                descriptionElement = self.get_child_element(entryParentel, self.ENTRY_DESCRIPTION_AFTER_GLOBAL_SEARCH_OLDUI)
-            except NoSuchElementException:
-                writeToLog("INFO","FAILED to find entry description element after global search")
-                return False
-            
-            if descriptionElement.text != description:
-                writeToLog("INFO","FAILED to verify entry description after global search")
-                return False
+        # Verify thumbnail qr code
+        result =  self.clsCommon.myMedia.getResultAfterSearch(entryName)
+        if result == False:
+            writeToLog("INFO","FAILED to find search word in global search")
+            return False
+        
+        sleep(1)
+        parent = result.find_element_by_xpath("..")
+        entryHref = parent.get_attribute("href")
+        entryId = entryHref.split("/")[len(entryHref.split("/"))-1]
+        
+        tmp_entryThum = (self.ENTRY_THUMBNAIL_AFTER_GLOBAL_SEARCH_IMG[0], self.ENTRY_THUMBNAIL_AFTER_GLOBAL_SEARCH_IMG[1].replace('ENTRY_ID', entryId))
+        
+        thumbElement = self.wait_element(tmp_entryThum, multipleElements=True)
+        if thumbElement == False:
+            writeToLog("INFO","FAILED to find entry thumbnail element after global search")
+            return False                
+        
+        sleep(2)
+        thumQrCode =  self.clsCommon.qrcode.resolveQrCode(self.clsCommon.qrcode.takeCustomQrCodeScreenshot(8.7,2.2,4.4,1.4))
+         
+        if thumQrCode != thumbQRCodeResult:
+            writeToLog("INFO","FAILED verify entry thumbnail QR code after global search")
+            return False
+        
+        # Verify entry description
+        tmp_entrydesc = (self.ENTRY_PARENT_DESCRIPTION_AFTER_GLOBAL_SEARCH_NEWUI[0], self.ENTRY_PARENT_DESCRIPTION_AFTER_GLOBAL_SEARCH_NEWUI[1].replace('ENTRY_ID', entryId))
+        try:
+            descriptionPerantElement = self.get_element(tmp_entrydesc)
+            descriptionElement = self.get_child_element(descriptionPerantElement, self.ENTRY_DESCRIPTION_AFTER_GLOBAL_SEARCH_NEWUI)
+        except NoSuchElementException:
+            writeToLog("INFO","FAILED to find entry description element after global search")
+            return False
+        
+        if descriptionElement.text != description:
+            writeToLog("INFO","FAILED to verify entry description after global search")
+            return False
             
         writeToLog("INFO","Success, entry matedata was verify after global search")    
         return True
