bca56315a94f6cdb9617fa29c78319d8fceeadac
CusHoria <45174452+CusHoria@users.noreply.github.com>
2019-03-14 12:18:32 +0200

    Increased showAllEntrie timeout Updated #4983 test case in order to workaround an issue Completed test case #4984

--- web/lib/myMedia.py
+++ web/lib/myMedia.py
@@ -1421,7 +1421,7 @@ class MyMedia(Base):
             return True
 
 
-    def showAllEntries(self, searchIn = enums.Location.MY_MEDIA, timeOut=75, afterSearch=False):
+    def showAllEntries(self, searchIn = enums.Location.MY_MEDIA, timeOut=120, afterSearch=False):
         # Check if we are in My Media page
         if searchIn == enums.Location.MY_MEDIA:
             tmp_table_size = self.MY_MEDIA_TABLE_SIZE
