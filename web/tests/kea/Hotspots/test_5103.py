b767b175cbb9d0dd1bb6591529066c34e71109d6
CusHoria <45174452+CusHoria@users.noreply.github.com>
2019-05-07 16:07:41 +0300

    Updated the collectQrOfSlidesFromPlayer function, in order to re take the QR Code list if less than two QR codes were found Fixed Hotspot test cases: 5103, 5110, 5114, 5151

--- web/tests/kea/Hotspots/test_5103.py
+++ web/tests/kea/Hotspots/test_5103.py
@@ -128,7 +128,7 @@ class Test:
             
             presentedHotspotsDetailsList = self.common.player.returnPresentedHotspotDetails()
             writeToLog("INFO","Step 11: Going to verify the hotspots from the " + self.entryName + " entry, after duplicating the " + self.hotspotOne[0])
-            if self.common.player.hotspotVerification(self.hotspotsDict, presentedHotspotsDetailsList) == False:
+            if self.common.player.hotspotVerification(self.hotspotsDictUpdatedTimeline, presentedHotspotsDetailsList) == False:
                 writeToLog("INFO","Step 11: FAILED to verify the hotspots from the " + self.entryName + " entry, after duplicating the " + self.hotspotOne[0])
                 return
             ##################################################################
