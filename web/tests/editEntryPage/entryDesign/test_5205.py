c11307f5355297b6097c2481acd0f4cffbaa9cd5
CusHoria <45174452+CusHoria@users.noreply.github.com>
2019-05-16 11:59:16 +0300

    Completed the following Entry Design test cases: #5475 Entry Design - Change the Theme and then Reset To Default #5476 Entry Design - Create custom Theme Created a new function, changeToDefaultEntryDisplay, in order to have the Entry Design to the default values Updated the changeEntryDisplay function, in order to support no background color selection

--- web/tests/editEntryPage/entryDesign/test_5205.py
+++ web/tests/editEntryPage/entryDesign/test_5205.py
@@ -40,7 +40,7 @@ class Test:
     # Variables used in order to specify the path of the video entry
     filePathVideo = localSettings.LOCAL_SETTINGS_MEDIA_PATH + r'\videos\QR_30_sec_new.mp4'
     cssFilePath   = localSettings.LOCAL_SETTINGS_MEDIA_PATH + r'\css\simple.css'
-    logoFilePath  = localSettings.LOCAL_SETTINGS_MEDIA_PATH + r'\images\kaltura_logo.png'
+    logoFilePath  = localSettings.LOCAL_SETTINGS_MEDIA_PATH + r'\images\automation.jpg'
         
     #run test as different instances on all the supported platforms
     @pytest.fixture(scope='module',params=supported_platforms)
